ea2d7a5f5ac16382e9441ff1bfe816e9b2add275
juliana ferraz <julianaferraz95@hotmail.com>
2022-08-24 19:44:40 -0300

    5 alteracao variavel

--- tarefaa-dia-2/pratica-1.js
+++ tarefaa-dia-2/pratica-1.js
@@ -3,3 +3,8 @@ let email = "julianaferraz95@hotmail.com"
 let profissao = "corretora de seguros"
 let idade = "26"
 let sonhodelugar = "Disney"
+console.log(nome)
+console.log(email) 
+console.log(profissao) 
+console.log(idade) 
+console.log(sonhodelugar) 
